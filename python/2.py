bde883ee0321d89bbcbf99c2e99db963ce303ceb
Yoshi <dleonavichus@mail.ru>
2023-10-20 21:30:22 +0300

    minor fix

--- python/2.py
+++ python/2.py
@@ -1,21 +1,18 @@
 def add_atms(n, k, distances):
     distances_dict = {key: [value] for key, value in enumerate(distances)}
-    sorted_dict = dict(
-        sorted(distances_dict.items(), key=lambda item: item[1], reverse=True))
 
     for _ in range(k):
         max_distance = max(distances)
         max_distance_index = distances.index(max_distance)
 
-        for key, value in sorted_dict.items():
+        for key, value in distances_dict.items():
             if max(value) == max_distance:
                 divider = len(value) + 1
                 avg = sum(value) / divider
-                sorted_dict[key] = [avg] * divider
+                distances_dict[key] = [avg] * divider
                 distances[max_distance_index] = avg
 
-    new_dict = {key: sorted_dict[key] for key in sorted(sorted_dict.keys())}
-    result = [item for i in new_dict.values() for item in i]
+    result = [item for i in distances_dict.values() for item in i]
     return result
 
 
@@ -26,4 +23,4 @@ distances = [
     int(input(f'Расстояние между банкоматами {i}-{i+1}: ')) for i in range(1, n)]
 
 result = add_atms(n, k, distances)
-print(result)
+print(*result, sep=', ')
